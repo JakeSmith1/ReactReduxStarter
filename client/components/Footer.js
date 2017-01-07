eb90d656621debf6404144100a0b9d6fc73bbd4c
JakeSmith1 <jakesmith5757@gmail.com>
2017-01-07 14:53:33 -0500

    file structure, webpack, redux, index.js, basic route, footer

---
+++ client/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="main-footer">
+        <p>Copyright &copy; 2016 Gregarious Giraffes</p>
+        <p>Support: julio5757@yahoo.com</p>
+    </footer>
+)
+
+export default Footer
